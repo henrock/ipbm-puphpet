67a0feb447c5e79b1d75a0e873a3091b493fe6c4
henrock <henrik.jerlenius@gmail.com>
2015-06-30 20:15:15 +0200

    Removed auto clones

--- puphpet/files/exec-once/clone_client.sh
+++ puphpet/files/exec-once/clone_client.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'Cloning from GitHub'
-git clone https://github.com/henrock/ipbm-client.git /var/www/ipbm
+#git clone https://github.com/henrock/ipbm-client.git /var/www/ipbm
 echo 'Finished!'
--- puphpet/files/exec-once/clone_server.sh
+++ puphpet/files/exec-once/clone_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'Cloning from GitHub'
-git clone https://github.com/henrock/ipbm-server.git /var/server
+#git clone https://github.com/henrock/ipbm-server.git /var/server
 echo 'Finished!'
