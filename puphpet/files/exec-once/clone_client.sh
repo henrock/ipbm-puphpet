c5ad25f6aa013989780668edfac6f5b0097de00c
henrock <henrik.jerlenius@gmail.com>
2015-06-30 15:39:27 +0200

    Changed dirs of git clones

---
+++ puphpet/files/exec-once/clone_client.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo 'Going into ./www'
+cd ./www
+echo 'Cloning from GitHub'
+git clone https://github.com/henrock/ipbm-client.git ipbm
+echo 'Finished!'
--- puphpet/files/exec-once/clone_server.sh
+++ puphpet/files/exec-once/clone_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo 'Going into /var/server'
-cd /var/server
+echo 'Going into ./server'
+cd ./server
 echo 'Cloning from GitHub'
 git clone https://github.com/henrock/ipbm-server.git .
 echo 'Finished!'
